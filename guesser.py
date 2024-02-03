1fefd6c196fb13a449ba66441ddae145b1cf556a
Chad-Mowbray <mowbray.chad@gmail.com>
2024-02-03 14:32:32 -0500

    initial

---
+++ guesser.py
@@ -0,0 +1,38 @@
+class Guesser:
+    
+    def __init__(self):
+        self.guesses = []
+        
+    def get_input(self, msg, input_func=input):
+        user_input = input_func(msg)
+        return user_input
+    
+    def validate_input(self, user_input):
+        return int(user_input)
+
+    def is_game_over(self):
+        if len(self.guesses) >= 3:
+            return True
+        return False
+    
+    def add_to_guesses(self, validated_input):
+        self.guesses.append(validated_input)
+        return self.guesses
+    
+    def display_msg(self, msg, display_func=print):
+        display_func(msg)
+            
+    def run(self):
+        inp = self.get_input("Enter a number: ")
+        validated = self.validate_input(inp)
+        if validated:
+            self.add_to_guesses(validated)
+        if self.is_game_over():
+            self.display_msg("Game is over")
+        else:
+            self.run()
+            
+            
+if __name__ == "__main__":
+    g = Guesser()
+    g.run()
---
+++ test_guesser.py
@@ -0,0 +1,25 @@
+import unittest
+from guesser import Guesser
+
+
+class TestGuesser(unittest.TestCase):
+    
+    def test_game_over(self):
+        g = Guesser()
+        g.guesses = [1,2,3,4]
+        is_over = g.is_game_over()
+        self.assertTrue(is_over)
+    
+    def test_valid_input(self):
+        g = Guesser()
+        with self.assertRaises(ValueError):
+            g.validate_input("ee")
+        self.assertEqual(g.validate_input("3"), 3)
+        
+    def test_get_input(self):
+        g = Guesser()
+        inp = g.get_input("hi", lambda x: "there")
+        self.assertEqual(inp, "there")
+        
+if __name__ == "__main__":
+    unittest.main()
